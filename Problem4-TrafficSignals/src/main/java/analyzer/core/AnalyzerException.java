11ed5869ff6ee62079c67c65246f33ce4b2903fe
Goutham <gouthigoud@gmail.com>
2018-01-15 17:05:23 -0500

    commiting Traffic Signals project

---
+++ Problem4-TrafficSignals/src/main/java/analyzer/core/AnalyzerException.java
@@ -0,0 +1,19 @@
+
+public class AnalyzerException extends RuntimeException {
+
+    public AnalyzerException(final String message) {
+        super(message);
+    }
+
+    public AnalyzerException(final String message, final String... args) {
+        super(String.format(message, (Object[]) args));
+    }
+
+    public AnalyzerException(Throwable throwable, String message) {
+        super(message, throwable);
+    }
+
+    public AnalyzerException(Throwable throwable, String message, String... args) {
+        this(throwable, String.format(message, (Object[]) args));
+    }
+}
